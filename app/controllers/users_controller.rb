5368bdd27e2df7e13d2d5ca11641129fc37eca10
qz <meshipsmou@yahoo.co.jp>
2023-02-19 01:17:16 +0900

    show追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      
+      redirect_to user_path(@user.id)
     else
       render :new
     end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -23,5 +24,5 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
+
 end
