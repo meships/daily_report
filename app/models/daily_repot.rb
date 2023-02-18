826e78aea99c101bdc7e6f4f86bbcec0143e5fd2
qz <meshipsmou@yahoo.co.jp>
2023-02-19 01:45:05 +0900

    日報db作成

---
+++ app/models/daily_repot.rb
@@ -0,0 +1,2 @@
+class DailyRepot < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :daily_reports
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
 end
