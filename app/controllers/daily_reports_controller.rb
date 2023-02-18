72cb35b9e9ad74ad5565b4aacc44ae05518682c8
qz <meshipsmou@yahoo.co.jp>
2023-02-19 01:54:25 +0900

    日報index new create作成

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -1,8 +1,23 @@
 class DailyReportsController < ApplicationController
+  before_action :login_required
   def index
+    @daily_reports = current_user.daily_reports
   end
 
   def new
+    @daily_report = DailyReport.new
+  end
+
+  def create
+    #binding.pry
+    @daily_report = current_user.daily_reports.build(daily_report_params)
+      if @daily_report.save
+
+        flash[:notice] = "日報を作成しました"
+        redirect_to daily_reports_path
+      else
+        render :new
+      end
   end
 
   def show
